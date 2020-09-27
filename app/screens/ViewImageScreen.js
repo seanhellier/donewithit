276d92dca7d4ba602066120e1b16e1b21ff9a21b
Sean Leif Hellier <sean@newtropolis.com>
2020-09-27 15:55:41 -0400

    added item page

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+
+function ViewImageScreen(props) {
+	return (
+		<Image
+			resizeMode="contain"
+			style={styles.image}
+			source={require("../assets/chair.jpg")}
+		/>
+	);
+}
+
+const styles = StyleSheet.create({
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+});
+
+export default ViewImageScreen;
