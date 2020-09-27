da994820c04a484a6c21f1a9c6fcc3109cd06773
Sean Leif Hellier <sean@newtropolis.com>
2020-09-27 11:32:15 -0400

    set app elements on page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ImageBackground, StyleSheet } from "react-native";
+import { View, ImageBackground, StyleSheet, Image } from "react-native";
 
 function WelcomeScreen(props) {
 	return (
@@ -7,18 +7,26 @@ function WelcomeScreen(props) {
 			style={styles.background}
 			source={require("../assets/background.jpg")}
 		>
+			<Image source={require("../assets/logo-red.png")} />
 			<View style={styles.loginButton}></View>
+			<View style={styles.registerButton}></View>
 		</ImageBackground>
 	);
 }
 const styles = StyleSheet.create({
 	background: {
 		flex: 1,
+		justifyContent: "flex-end",
 	},
 	loginButton: {
-		width: "100",
+		width: "100%",
 		height: 70,
 		backgroundColor: "#fc5c65",
+	},
+	registerButton: {
+		width: "100%",
+		height: 70,
+		backgroundColor: "#4ecdc4",
 	},
 });
 
