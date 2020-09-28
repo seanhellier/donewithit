de6295d80e65c7acb6c305ba2be56c0dac5f237d
Sean Leif Hellier <sean@newtropolis.com>
2020-09-28 14:02:18 -0400

    created custom font styles linked via separate component file

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import AppText from "./app/components/AppText";
 
 export default function App() {
-	return <ViewImageScreen />;
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text>I love React Native</Text>
+		</View>
+	);
 }
