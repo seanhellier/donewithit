5728bb9778efad00836089dea2d4f66ea4ae7f21
Sean Leif Hellier <sean@newtropolis.com>
2020-10-11 16:33:40 -0400

    input components - exercise 3

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { View, Image } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput } from "react-native";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import Login from "./app/screens/Login";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
+// import Login from "./app/screens/Login";
+// import Card from "./app/components/Card";
+// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import MessagesScreen from "./app/screens/MessagesScreen";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-	return <MessagesScreen />;
+	const [firstName, setFirstName] = useState("");
+
+	return (
+		<Screen>
+			<Text>{firstName}</Text>
+			<TextInput
+				maxLength={5}
+				keyboardType="numeric"
+				clearButtonMode="always"
+				secureTextEntry
+				onChangeText={(text) => setFirstName(text)}
+				placeholder="First Name"
+				style={{
+					borderBottomColor: "#ccc",
+					borderBottomWidth: 1,
+				}}
+			/>
+		</Screen>
+	);
+
+	// <ListingDetailsScreen />;
 }
